b07e65cbbdc50104adfdf2ab0405728553bf4c95
jerrymares <jerrymares@gmail.com>
2026-08-24 07:58:41 -0500

    style: format BC capability table query

--- src/video_core/renderer_vulkan/vk_instance.cpp
+++ src/video_core/renderer_vulkan/vk_instance.cpp
@@ -703,8 +703,8 @@ void Instance::CollectImageFormatInfo() {
                                                         vk::ImageUsageFlagBits::eSampled;
     constexpr vk::ImageCreateFlags compressed_1d_flags =
         vk::ImageCreateFlagBits::eMutableFormat | vk::ImageCreateFlagBits::eExtendedUsage;
-    supports_compressed_1d_images = std::ranges::all_of(
-        LiverpoolToVK::SurfaceFormats(), [this](const auto& surface_format) {
+    supports_compressed_1d_images =
+        std::ranges::all_of(LiverpoolToVK::SurfaceFormats(), [this](const auto& surface_format) {
             if (!AmdGpu::IsBlockCoded(surface_format.data_format)) {
                 return true;
             }
